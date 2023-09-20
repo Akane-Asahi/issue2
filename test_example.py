c55dd5cb708c2e7ed59b5cc1fcac3e0e3e9e5640
unknown <Akane-Asahi>
2023-09-21 02:44:04 +0600

    1st commit

---
+++ test_example.py
@@ -0,0 +1,13 @@
+# test_example.py
+
+import unittest
+
+class TestExample(unittest.TestCase):
+    def test_pass(self):
+        self.assertEqual(1, 1)
+
+    def test_another_pass(self):
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertEqual(1, 2)
